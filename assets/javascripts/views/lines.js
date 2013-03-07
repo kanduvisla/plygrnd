1a07754c1f542a479ab69274f9ec2a44c3a1c4d9
Giel Berkers <giel@twisted.nl>
2013-03-07 17:33:51 +0100

    inital start of 'lines'

---
+++ assets/javascripts/views/lines.js
@@ -0,0 +1,46 @@
+// Play with a line:
+
+siteApp.views.push({
+    name            : "Basic View",
+    // Variables used by this view:
+    vars            : {
+        step : 0,
+        segments : 10
+    },
+    // Initialisation:
+    initFunction    : function(ctx, vars){
+        vars.step = 0;
+    },
+    // Stepping function:
+    stepFunction    : function(ctx, vars){
+        // Clear first:
+        ctx.clearRect(0, 0, ctx.width, ctx.height);
+
+        vars.step += 0.01;
+        ctx.strokeStyle = '#fff';
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+
+        var fromX = 0;
+        var fromY = ctx.height/2;
+        ctx.moveTo(fromX, fromY);
+        var segmentWidth = ctx.width / (vars.segments);
+
+        for(var i=1; i<vars.segments; i++)
+        {
+
+            var p = i/vars.segments;
+            if(p>.5) { p = 1-p; }
+
+            var x = i * segmentWidth;
+            var y = (ctx.height/2) + (Math.sin(vars.step + i) * (100 * p));
+            ctx.lineTo(i * segmentWidth, y);
+
+            fromX = x;
+            fromY = y;
+        }
+
+        ctx.lineTo(ctx.width, ctx.height/2);
+        ctx.stroke();
+    }
+});
